66d437ba7cfee3fef2bba8e658ab0ac52222b294
TranThienVan <s3740845@rmit.edu.vn>
2021-07-25 21:18:31 +0700

    Fix the search icon for the navbar

--- src/Components/Navbar/SearchBox/index.js
+++ src/Components/Navbar/SearchBox/index.js
@@ -10,8 +10,9 @@ const Index = () => {
             placeholder="Search"
             className="mr-2"
             aria-label="Search"
+            style={{backgroud:"Transparent", border: "none"}}
           />
-          <Button variant="outline-dark" style={{marginRight: "10px", marginLeft: "6px"}}><i className="fa fa-search"></i></Button>
+          <Button variant="outline-light" style={{marginRight: "10px", marginLeft: "6px", border: "none"}}><i className="fa fa-search"></i></Button>
 
       </Form>
     </>
