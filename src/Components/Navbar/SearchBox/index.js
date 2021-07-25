ed5ca31ad7d84443ff7b2b8e1f7f7f06a628ea0c
Duc Anh (Ryan) Nguyen <nduc.anh@gmail.com>
2021-07-26 01:02:31 +0700

    submit without search function

--- src/Components/Navbar/SearchBox/index.js
+++ src/Components/Navbar/SearchBox/index.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import {Form, FormControl, Button } from 'react-bootstrap'
 
-const Index = () => {
+const SearchBox = () => {
+  const transparentStyle = {
+    'background-color': 'transparent', 
+    'border': '1px solid white'
+  };
+
   return (
     <>
       <Form className="d-flex">
           <FormControl
+            style={transparentStyle}
             type="search"
-            placeholder="Search"
-            className="mr-2"
+            placeholder=""
+            className="mr-2 transparent-input"
             aria-label="Search"
           />
-          <Button variant="outline-dark" style={{marginRight: "10px", marginLeft: "6px"}}><i className="fa fa-search"></i></Button>
+          <Button variant="outline-light" style={{marginRight: "10px", marginLeft: "6px"}}><i className="fa fa-search"></i></Button>
 
       </Form>
     </>
   )
 }
 
-export default Index
+export default SearchBox;
---
+++ src/Pages/SearchPage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css"
+
+function SearchPage() {
+  return (
+    <div className="SearchPage">
+      Boohoo!
+    </div>
+  );
+}
+
+export default SearchPage; 
